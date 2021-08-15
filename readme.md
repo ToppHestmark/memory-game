bd2a6481d42e08fd2be3dc6afc5894116a50e81b
Suphachai Hestmark <top_hestmark@yahoo.no>
2021-08-15 11:41:43 +0200

    Completed the winning logic

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # Memory Game
 
 Simple memory game with Typescript and React.
+
+### Todos
+
+- [ ] Generate amount of attemp user clicked counter.
+- [ ] Implement a logic after all cards has been flipped.
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./components/Card";
 
 import { createBoard, CardType } from "./setup";
@@ -7,6 +7,8 @@ import { shuffleArray } from "./utils";
 import { Grid } from "./App.styles";
 
 const App = () => {
+  const TIMEOUT_CARD = 1000;
+
   const [cards, setCards] = useState<CardType[]>(shuffleArray(createBoard()));
   const [gameWon, setGameWon] = useState(false);
   const [matchedPairs, setMatchedPairs] = useState(0);
@@ -14,8 +16,55 @@ const App = () => {
     undefined
   );
 
-  const handleCardClick = () => {
-    console.log("Clicked");
+  useEffect(() => {
+    if (matchedPairs === cards.length / 2) {
+      console.log("You WIN!");
+      setGameWon(true);
+    }
+  }, [matchedPairs]);
+
+  const handleCardClick = (currentlySelectedCard: CardType) => {
+    // Flip the card
+    setCards((prev) =>
+      prev.map((card) =>
+        card.id === currentlySelectedCard.id
+          ? { ...card, flipped: true, clickable: false }
+          : card
+      )
+    );
+
+    // If this first card is flipped, keep it flipped
+    if (!clickedCard) {
+      setClickedCard({ ...currentlySelectedCard });
+      return;
+    }
+
+    // Checked the second selected card for match
+    if (clickedCard.matchingCardId === currentlySelectedCard.id) {
+      setMatchedPairs((prev) => prev + 1);
+      setCards((prev) =>
+        prev.map((card) =>
+          card.id === clickedCard.id || card.id === currentlySelectedCard.id
+            ? { ...card, clickable: false }
+            : card
+        )
+      );
+      setClickedCard(undefined);
+      return;
+    }
+
+    // If not matched pairs, wait one sec and flip the card back
+    setTimeout(() => {
+      setCards((prev) =>
+        prev.map((card) =>
+          card.id === clickedCard.id || card.id === currentlySelectedCard.id
+            ? { ...card, flipped: false, clickable: true }
+            : card
+        )
+      );
+    }, TIMEOUT_CARD);
+
+    setClickedCard(undefined);
   };
 
   return (
